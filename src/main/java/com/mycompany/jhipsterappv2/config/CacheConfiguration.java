a30b6f85eba5616450320e980062ce672e057226
Michael Gilbertson <mggwxyz@gmail.com>
2017-05-09 09:37:44 -0400

    Updated some files

--- src/main/java/com/mycompany/jhipsterappv2/config/CacheConfiguration.java
+++ src/main/java/com/mycompany/jhipsterappv2/config/CacheConfiguration.java
@@ -42,10 +42,6 @@ public class CacheConfiguration {
             cm.createCache(com.mycompany.jhipsterappv2.domain.User.class.getName() + ".authorities", jcacheConfiguration);
             cm.createCache(com.mycompany.jhipsterappv2.domain.PersistentToken.class.getName(), jcacheConfiguration);
             cm.createCache(com.mycompany.jhipsterappv2.domain.User.class.getName() + ".persistentTokens", jcacheConfiguration);
-            cm.createCache(com.mycompany.jhipsterappv2.domain.Author.class.getName(), jcacheConfiguration);
-            cm.createCache(com.mycompany.jhipsterappv2.domain.Author.class.getName() + ".titles", jcacheConfiguration);
-            cm.createCache(com.mycompany.jhipsterappv2.domain.Book.class.getName(), jcacheConfiguration);
-            cm.createCache(com.mycompany.jhipsterappv2.domain.Author.class.getName() + ".names", jcacheConfiguration);
             // jhipster-needle-ehcache-add-entry
         };
     }
--- src/main/webapp/app/entities/entity.module.ts
+++ src/main/webapp/app/entities/entity.module.ts
@@ -1,13 +1,9 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
-import { Jhipsterappv3AuthorModule } from './author/author.module';
-import { Jhipsterappv3BookModule } from './book/book.module';
 /* jhipster-needle-add-entity-module-import - JHipster will add entity modules imports here */
 
 @NgModule({
     imports: [
-        Jhipsterappv3AuthorModule,
-        Jhipsterappv3BookModule,
         /* jhipster-needle-add-entity-module - JHipster will add entity modules here */
     ],
     declarations: [],
